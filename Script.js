9c99923031002924ce01010dc81f573cb1ccaf73
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2022-12-19 14:09:43 +0500

    Portfolio

---
+++ Script.js
@@ -0,0 +1,21 @@
+function facebook()
+{
+    window.open("https://www.facebook.com/shayankhan.mahar.1","_blank")
+}
+
+function instagram()
+{
+    window.open("https://www.instagram.com/shayan_khan_mahar/","_blank")
+}
+function twitter()
+{
+    window.open("https://www.twitter.com/shayankhanmahar","_blank")
+}
+function linkedin()
+{
+    window.open("https://www.linkedin.com/in/muhammad-alamzeb-mahar-5bb01b233?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Be0frumfPTMyxi%2BH9AcSFdQ%3D%3D","_blank")
+}
+function snapchat()
+{
+    window.open("https://www.snapchat.com/add/shayanmahar?share_id=y-SLYo5fRMc&locale=en-US","_blank")
+}
